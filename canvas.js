744d2ed7e2c7966d9e6a6dd4a904bba3f6433f1d
J0SLAN <J0SLAN@users.noreply.github.com>
2017-04-30 05:36:14 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,12 @@
-
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+canvas.width = 640;
+canvas.height= 480;
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(50,50);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(100,100);
+ctx.arc(100,100,20,Math.PI);
+ctx.stroke();
